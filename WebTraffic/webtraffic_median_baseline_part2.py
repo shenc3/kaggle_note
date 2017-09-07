79bbdd172e9209df3db8fc47bfea6b61f0d42760
shenc3 <shenc3@126.com>
2017-09-08 00:03:13 +0800

    add a fillna at the end of sub.

--- WebTraffic/webtraffic_median_baseline_part2.py
+++ WebTraffic/webtraffic_median_baseline_part2.py
@@ -6,12 +6,12 @@ import numpy as np
 
 
 print("导入数据...")
-DIR = r'D:\PlayGround\kaggle\WebTraffic'
+DIR = r'E:\Projects\kaggle\WebTraffic'
 train_visits = pd.read_csv(os.path.join(DIR, 'train_median_60days.csv'), encoding='gbk')
 keys = pd.read_csv(os.path.join(DIR, 'key_1.csv'))
 
 keys['Page'] = keys['Page'].str[:-11]
-keys = keys.merge(train_visits, how='left', on='Page')
+keys = keys.merge(train_visits, how='left', on='Page').fillna(0)
 
 print("保存输出submission...")
 keys[['Id', 'Visits']].to_csv(os.path.join(DIR, 'sub_median_base.csv'), index=False)
